f37f4638818747fb0c2cecb3e17f8e7b7eb576ba
KyoungJea_Park <KyoungJea_Park@172.21.224.1>
2020-10-16 16:48:04 +0900

    원격 실행으로 바꿈

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-docker-compose down && \
 chmod +x gradlew && \
-./gradlew clean build && \
-docker-compose build && \
-docker image prune -f
-
-BUILD_ID=dontKillMe docker-compose up -d
-JENKINS_NODE_COOKIE=dontKillMe docker-compose up -d
-docker-compose up -d
+./gradlew clean build
