58fb81a5e4c4bdc3c6b30bb38aeef7a3b45bf4c7
KyoungJea_Park <KyoungJea_Park@172.21.224.1>
2020-10-16 15:01:44 +0900

    dont't kill me plz...

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ chmod +x gradlew && \
 docker-compose build && \
 docker image prune -f
 
-sh 'JENKINS_NODE_COOKIE=dontKillMe ./start.sh'
+BUILD_ID=dontKillMe
+JENKINS_NODE_COOKIE=dontKillMe
+sh ./start.sh
