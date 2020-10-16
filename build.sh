c68a308d0f3feeaeced0fc86840d2aa8ceb7dcbc
KyoungJea_Park <KyoungJea_Park@172.21.224.1>
2020-10-16 14:33:14 +0900

    다시 도전!

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ docker-compose down && \
 chmod +x gradlew && \
 ./gradlew clean build && \
 docker-compose build && \
-docker image prune -f && \
-BUILD_ID=dontKillMe sh ./start.sh
+docker image prune -f
+
+BUILD_ID=dontKillMe ./start.sh
+sh ./start.sh
