2ed303fa9ba6b894f644a4883678239773b29125
KyoungJea_Park <KyoungJea_Park@172.21.224.1>
2020-10-16 12:20:04 +0900

    build.sh 수정

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #/bin/bash
-docker-compose down
-./gradlew bootjar
-docker-compose build
-docker image prune -f
+
+docker-compose down && \
+./gradlew bootjar && \
+docker-compose build && \
+docker image prune -f && \
 docker-compose up -d
