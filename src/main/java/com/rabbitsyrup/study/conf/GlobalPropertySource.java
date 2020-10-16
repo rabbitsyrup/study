a6d98b5de6ec5302c5f01e468ec3e5d43bf4ce84
KyoungJea_Park <KyoungJea_Park@172.21.224.1>
2020-10-16 13:34:06 +0900

    gitignore가 잘못되어 올라가야 될 파일이 누락됨

---
+++ src/main/java/com/rabbitsyrup/study/conf/GlobalPropertySource.java
@@ -0,0 +1,39 @@
+package com.rabbitsyrup.study.conf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("file:/conf/config.properties")
+public class GlobalPropertySource {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    
+    @Value("${spring.datasource.url}")
+    private String url;
+    
+    @Value("${spring.datasource.username}")
+    private String username;
+    
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
